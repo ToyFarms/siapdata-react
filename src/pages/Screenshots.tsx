937b93958d5f538a9c927d832052a7613f5efc14
ToyFarms <ToyFarms@users.noreply.github.com>
2025-09-11 09:21:17 +0700

    Remove next, invisible navbar when dock

--- src/pages/Screenshots.tsx
+++
@@ -1,9 +0,0 @@
-import ScreenshotCarousel from "@/components/ScreenshotCarousel";
-
-export default function Screenshots() {
-  return (
-    <div className="px-[20%]">
-      <ScreenshotCarousel />
-    </div>
-  );
-}
